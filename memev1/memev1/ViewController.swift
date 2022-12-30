d5eb4afb47e6692524549d18c01bd1a7a683581d
luancaius <luan.ca@outlook.com>
2022-12-30 16:35:59 -0500

    adding camera button

--- memev1/memev1/ViewController.swift
+++ memev1/memev1/ViewController.swift
@@ -8,12 +8,46 @@
 import UIKit
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate,
-                        UINavigationControllerDelegate {
+                        UINavigationControllerDelegate, UITextFieldDelegate {
 
+    @IBOutlet weak var bottomText: UITextField!
+    
     @IBOutlet weak var imagePickerView: UIImageView!
+    @IBOutlet weak var topText: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        let memeTextAttributes: [NSAttributedString.Key: Any] = [
+            NSAttributedString.Key.strokeColor: UIColor.black,
+            NSAttributedString.Key.foregroundColor: UIColor.white,
+            NSAttributedString.Key.font: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+            NSAttributedString.Key.strokeWidth:  -2.0
+        ]
+        topText.defaultTextAttributes = memeTextAttributes
+        topText.text = "TOP"
+        topText.textAlignment = .center
+        topText.delegate = self
+        
+        bottomText.defaultTextAttributes = memeTextAttributes
+        bottomText.text = "BOTTOM"
+        bottomText.textAlignment = .center
+        bottomText.delegate = self
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        print("inside text", textField)
+        if textField.tag == 0 {
+            topText.text = ""
+        }
+        if textField.tag == 1 {
+            bottomText.text = ""
+        }
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
     }
 
     @IBAction func pickAnImage(_ sender: Any) {
@@ -22,5 +56,20 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate,
             present(imagePicker, animated: true, completion: nil)
     }
     
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        imagePickerView.image = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    @IBAction func takePicture(_ sender: Any) {
+        let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        present(imagePicker, animated: true, completion: nil)
+
+    }
 }
 
