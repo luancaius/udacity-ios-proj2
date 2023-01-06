a967900f0c3aa12b42dad7f57eff9e2b281cf3d8
luancaius <luan.ca@outlook.com>
2023-01-05 21:31:05 -0500

    fixing keyboard issues

--- memev1/memev1/ViewController.swift
+++ memev1/memev1/ViewController.swift
@@ -35,6 +35,46 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate,
         bottomText.delegate = self
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+
+        super.viewWillAppear(animated)
+        subscribeToKeyboardNotifications()
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+
+        super.viewWillDisappear(animated)
+        unsubscribeFromKeyboardNotifications()
+    }
+    
+    func subscribeToKeyboardNotifications() {
+
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+
+    func unsubscribeFromKeyboardNotifications() {
+
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+
+    }
+    @objc func keyboardWillShow(_ notification:Notification) {
+        if bottomText!.isEditing {
+            view.frame.origin.y -= getKeyboardHeight(notification)
+        }
+    }
+
+    @objc func keyboardWillHide(_ notification:Notification) {
+        view.frame.origin.y = 0
+    }
+    
+    func getKeyboardHeight(_ notification:Notification) -> CGFloat {
+
+        let userInfo = notification.userInfo
+        let keyboardSize = userInfo![UIResponder.keyboardFrameEndUserInfoKey] as! NSValue // of CGRect
+        return keyboardSize.cgRectValue.height
+    }
     func textFieldDidBeginEditing(_ textField: UITextField) {
         print("inside text", textField)
         if textField.tag == 0 {
